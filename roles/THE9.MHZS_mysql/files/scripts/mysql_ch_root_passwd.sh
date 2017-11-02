cefef1b4c4aa991e2331667c9a153c91bb220116
root <root@GM.(none)>
2017-11-02 17:24:40 +0800

    tested on production env

---
+++ roles/THE9.MHZS_mysql/files/scripts/mysql_ch_root_passwd.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+dbpasswd=`cat ~/.mysql_secret | grep password | awk '{print $18}'`
+if [ -z $dbpasswd ]; then
+	/usr/bin/mysql -uroot  -e "set password=PASSWORD('Abcd=2345'); set password for 'root'@'localhost'=PASSWORD('Abcd=2345'); set password for 'root'@'127.0.0.1'=PASSWORD('Abcd=2345');"
+else
+	/usr/bin/mysql -uroot -p$dbpasswd -e "set password=PASSWORD('Abcd=2345'); set password for 'root'@'localhost'=PASSWORD('Abcd=2345');set password for 'root'@'127.0.0.1'=PASSWORD('Abcd=2345');"
+fi
+
